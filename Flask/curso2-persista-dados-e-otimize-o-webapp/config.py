8102e625ce2110c1a98ca5865f90e251538c3945
matheus-cortez <mrctcortez@gmail.com>
2023-01-20 13:59:51 -0300

    Implementando imagens nos jogos, solucionando problemas de cache com a utilização das imagens

--- Flask/curso2-persista-dados-e-otimize-o-webapp/config.py
+++ Flask/curso2-persista-dados-e-otimize-o-webapp/config.py
@@ -1,3 +1,5 @@
+import os
+
 #app.secret_key = 'alura'
 SECRET_KEY = 'alura'
 #app.config['SQLALCHEMY_DATABASE_URI'] = \
@@ -9,3 +11,5 @@ SQLALCHEMY_DATABASE_URI = \
         servidor = 'localhost', # ou 127.0.0.1
         database = 'jogoteca'
     )
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
--- Flask/curso2-persista-dados-e-otimize-o-webapp/views.py
+++ Flask/curso2-persista-dados-e-otimize-o-webapp/views.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
+from helpers import recupera_imagem, deleta_arquivo
+import time
 
 @app.route('/')
 def index():
@@ -34,6 +36,11 @@ def criar():
     db.session.add(novo_jogo)
     db.session.commit()
 
+    arquivo = request.files['arquivo'] # request.files para ler arquivos
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}-{timestamp}.jpg') # poderiamos usar {arquivo.filename} 
+
     return redirect(url_for('index'))
 
 @app.route('/editar/<int:id>')
@@ -41,7 +48,8 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = Jogos.query.filter_by(id=id).first()
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+    capa_jogo = recupera_imagem(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
@@ -52,6 +60,12 @@ def atualizar():
 
     db.session.add(jogo)
     db.session.commit()
+
+    arquivo = request.files['arquivo'] # request.files para ler arquivos
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time() # usamos o timestamp para contornar o problema do cache armazenar uma imagem desatualizada
+    deleta_arquivo(jogo.id) # deleta a imagem existente para não ter duas imagens com o mesmo id de jogo
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg') # poderiamos usar {arquivo.filename} 
 
     return redirect(url_for('index'))
 
@@ -93,3 +107,7 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
