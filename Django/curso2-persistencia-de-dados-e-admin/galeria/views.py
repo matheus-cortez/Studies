014064feba2c2dfbd8ed4780dc0c55296c2fe584
matheus-cortez <mrctcortez@gmail.com>
2023-02-03 15:47:07 -0300

    Tópico 1 concluído

---
+++ Django/curso2-persistencia-de-dados-e-admin/galeria/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    dados = {
+    1: {"nome": "Nebulosa de Carina",
+        "legenda": "webbtelescope.org / NASA / James Webb"},
+    2: {"nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"}
+}
+    # o render nos permite passar dados além de renderizar
+    return render(request, 'galeria/index.html', {"cards": dados})
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
